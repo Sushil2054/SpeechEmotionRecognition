74fc960bfa4ed05811be6829a4d92c18af8e2176
Ajaya Thakur <ajayathakur383@gmail.com>
2020-10-16 17:35:03 +0545

    second commit full

--- ravdessDB_preparation.py
+++ ravdessDB_preparation.py
@@ -7,46 +7,12 @@ import glob
 DATASET_PATH = "C:\\Users\\PREDATOR\\Desktop\\project\\code\\ravdess dataset"
 JSON_PATH = "data.json"
 SAMPLE_RATE = 22050
+
 TRACK_DURATION = 0.03 # measured in seconds
 SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
-emotions = {
-    '01':'neutral',
-    '02':'calm',
-    '03':'happy',
-    '04':'sad',
-    '05':'angry',
-    '06':'fearful',
-    '07':'disgust',
-    '08':'surprised'
-}
-filename= []
-emotion = []
 
-
-
-for file in glob.glob("C:\\Users\\PREDATOR\\Desktop\\project\\code\\ravdess dataset\\Actor_*\\*.wav"):
-    file_name = os.path.basename(file)
-    filename.append(file_name)
-    emotion.append(emotions[file_name.split("-")[2]])
-    
-print(filename)
-print(emotion)
-
-
-
-
-"""
 def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
-    """"""Extracts MFCCs from music dataset and saves them into a json file along witgh genre labels.
-        :param dataset_path (str): Path to dataset
-        :param json_path (str): Path to json file used to save MFCCs
-        :param num_mfcc (int): Number of coefficients to extract
-        :param n_fft (int): Interval we consider to apply FFT. Measured in # of samples
-        :param hop_length (int): Sliding window for FFT. Measured in # of samples
-        :param: num_segments (int): Number of segments we want to divide sample tracks into
-        :return:
-        """"""
 
     # dictionary to store mapping, labels, and MFCCs
     data = {
@@ -58,25 +24,27 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
     samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
     num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
 
+    
     # loop through all genre sub-folder
     for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
-            
-        # ensure we're processing a genre sub-folder level
+        #print(f"{filenames}\n")
         if dirpath is not dataset_path:
+                
+            # for filename in filenames:
+            #     semantic_label = filename.split("-")[2]
+            #     data["labels"].append(semantic_label)
+  
+# process all audio files in genre sub-dir
+            for i, f in enumerate(filenames):
+                
 
-            # save genre label (i.e., sub-folder name) in the mapping
-            semantic_label = dirpath.split("/")[-1]
-            data["mapping"].append(semantic_label)
-            print("\nProcessing: {}".format(semantic_label))
-
-            # process all audio files in genre sub-dir
-            for f in filenames:
-
-		# load audio file
+                labels = f.split("-")[2]
+                # load audio file
                 file_path = os.path.join(dirpath, f)
+                #print(file_path)
                 signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
 
-                # process all segments of audio file
+                        # process all segments of audio file
                 for d in range(num_segments):
 
                     # calculate start and finish sample for current segment
@@ -90,13 +58,46 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
                     # store only mfcc feature with expected number of vectors
                     if len(mfcc) == num_mfcc_vectors_per_segment:
                         data["mfcc"].append(mfcc.tolist())
-                        data["labels"].append(i-1)
-                        print("{}, segment:{}".format(file_path, d+1))
+                        #print(labels)
+                        #print("==========================================")
+                        data["labels"].append(labels)
+                        #print("{}, segment:{}".format(file_path, d+1))
+
+          
+    data["mapping"] = list(set(data["labels"]))
+    data["mapping"].sort()
+    print(data["labels"])
+    #print(len(data["labels"]))
+        #print(data["mapping"])
+        #print(len(data["mapping"]))
+
+    for i, label in enumerate(data["mapping"]):
+        if label == '01':
+            data["mapping"][i] = 'Neutral'
+        if label == '02':
+            data["mapping"][i] = 'Calm'
+        if label == '03':
+            data["mapping"][i] = 'Happy'
+        if label == '04':
+            data["mapping"][i] = 'Sad'
+        if label == '05':
+            data["mapping"][i] = 'Angry'
+        if label == '06':
+            data["mapping"][i] = 'Fearful'
+        if label == '07':
+            data["mapping"][i] = 'Disgust'
+        if label == '08':
+            data["mapping"][i] = 'Surprised'
+
+    #print(data["mapping"])
+
 
     # save MFCCs to json file
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
-        
+    
+    print(len(data["labels"]))
+
         
 if __name__ == "__main__":
-    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)"""
+    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
